dc46b42f41b0ba32a4364760395711a7ead95bef
Songwhan Han <hansh1029@gmail.com>
2020-12-26 00:52:44 +0900

    set apollo client

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { ApolloProvider } from '@apollo/client';
+import client from "./apollo.js";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<ApolloProvider client={client}><App /></ApolloProvider>, document.getElementById("root"));
